c43ab3e7c71a3e1daa97d9ff7d691a8dcc92126c
Ivan Hermawan <ivanhermawan19@gmail.com>
2026-08-02 11:03:27 +0700

    test

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import app from './src/app.js';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Healthy chatbot API listening on http://localhost:${port}`);
+  console.log(`Healthy chatbot test API listening on http://localhost:${port}`);
 });
